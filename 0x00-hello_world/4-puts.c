4715cc86a792158c1c29e0f5230bc7af34f5ecd0
rassane <an.ghassane@gmail.com>
2023-03-16 11:17:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main entry point (does it ven matter)
- *
+ * retun alwawysok
  */
-int main (void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
