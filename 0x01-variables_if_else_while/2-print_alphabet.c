a4940bb7f98a55eac0e41beb13bccad55d03513c
rassane <an.ghassane@gmail.com>
2023-03-17 14:43:59 -0700

    abc

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+main - enter here
+return: success BS all 0 reur
+*/
+int main(void)
+{
+	char n = 'a'
+
+		while (n <= 'z')
+		{
+			putchar(n);
+			n++;
+	       	}
+       	return(0);
+}
