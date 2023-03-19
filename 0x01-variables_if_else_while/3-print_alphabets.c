647a5d9e2dc0a77d89d9243a1d4a47527b7151da
rassane <an.ghassane@gmail.com>
2023-03-19 05:34:31 -0700

    qe

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,18 +5,18 @@
 */
 int main(void)
 {
-        char n = 'a';
-        char N = 'A';
-                while (n <= 'z')
-                {
-                        putchar(n);
-                        n++;
-                }
-                while (N <= 'Z')
-                {
-                        putchar(N);
-                        N++;
-                }                
-                putchar('\n');
-        return (0);
+	char n = 'a';
+	char N = 'A';
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	while (N <= 'Z')
+	{
+		putchar(N);
+		N++;
+	}                
+	putchar('\n');
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - enter here more desc
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n = 'a';
+
+		while (n <= 'z' && n != 'e' && n != 'q')
+		{
+			putchar(n);
+			n++;
+		}
+		putchar('\n');
+	return (0);
+}
