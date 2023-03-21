bdd17f5f57382c8569a3352b7bb38816011c8f99
rassane <an.ghassane@gmail.com>
2023-03-21 12:42:02 -0700

    void print_alphabet(void);

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,14 @@
  * main - the entrance to whatever
  * Return: 0 return success
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char n='a';
 
 	while (n < 'z')
 	{
 	_putchar(n);
+	n++;
 	if (n == 'z')
 	_putchar('\n');
 	}
