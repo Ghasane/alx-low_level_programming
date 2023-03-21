2f9c01929c8759a3cc903a9241294f13e21805d6
rassane <an.ghassane@gmail.com>
2023-03-21 15:17:49 -0700

    jack_bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * jack_bauer - show the abc's
+ * @c: get corespanadin ascii to entered char
+ * Return: 1 if lower 0 if not
+ */
+void jack_bauer(void)
+{
+	int h=0;
+	int h1;
+	int m=0;
+	int m1=0;
+	int i=0;
+
+	while(h < 3)
+	{
+		while(h1 < 10)
+		{
+			while(m < 6)
+			{
+				while(m1 < 10)
+				{
+					_putchar(48 + h);
+					_putchar(48 + h1);
+					_putchar(':');
+					_putchar(48 + m);
+					_putchar(48 + m1);
+					if (h == 2 && h1 == 3 && m == 5 && m1 == 9)
+						return;
+					m1++;
+				}
+				m++;
+				m1 = 0;
+			}
+			h1++;
+			m = 0;
+			m1 = 0;
+		}
+		h++;
+		h1 = 0;
+		m = 0;
+		m1 = 0;
+	}
+		return;
+}
