364a7532183c0bb7eff03712aff7480a9df333bb
rassane <an.ghassane@gmail.com>
2023-03-21 12:31:43 -0700

    _putchar('_');

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
 #include "main.h"
 /**
  * main - the entrance to whatever
- * return: return success
+ * Return: 0 return success
 */
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-	return(0);
+	return (0);
 }
