057a59f9312958c9d3c1921181b87ff281c2c91a
rassane <an.ghassane@gmail.com>
2023-03-21 14:40:11 -0700

    int print_last_digit(int);

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int print_last_digit(int); - show the abc's
+ * @n: get nummber
+ * Return: 1 if pos 0 if 0 and -1 if neg
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	r = n % 1000;
+	return(r);
+}
