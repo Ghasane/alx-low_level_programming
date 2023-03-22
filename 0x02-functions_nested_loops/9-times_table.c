85a2580b42ff4ca38aad49a83f74d835d4504d73
rassane <an.ghassane@gmail.com>
2023-03-22 11:13:18 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,24 +3,33 @@
  * void times_table - show the abc'
  * Return: 1 if lower 0 if not
  */
-void times_table(void);
+void times_table(void)
 {
 	int a = 0;
 	int b = 0;
+	int x = 0;
+	int r = 0;
 
-	while (a < 9)
+	while (a < 10)
 	{
-		while (b < 9)
+		while (b < 10)
 		{
-			_putchar('0' + a * b);
+			r = (a * b) / 10;
+			x = (a * b) % 10;
+			if (r != 0)
+				_putchar('0' + r);
+			else 
+				_putchar(' ');
+			_putchar('0' + x);
+			if (b == 9)
+				break;
 			_putchar(',');
 			_putchar(' ');
 			b++;
-			if (b == 9)
-				_putchar('0' + a * b);
 		}
 		_putchar('\n');
 		a++;
+		b = 0;
 	}
 		return;
 }
