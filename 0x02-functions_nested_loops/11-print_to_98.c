a193c02a5f6e5f7ba8d1a89d467d335331af7d3d
rassane <an.ghassane@gmail.com>
2023-03-22 12:10:36 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * print_to_98 - show the abc's
+ * @n: entrered int
+ */
+void print_to_98(int n)
+{
+	int x = n;
+
+	if(n < 98)
+	{
+		while(x != 98)
+		{
+			printf("%d, ", x);
+			x++;
+		}
+		printf("98");
+	}
+	else if(n > 98)
+	{
+		while
+		{
+			printf("%d, ", x);
+			x--;
+		}
+		printf("98");
+	}
+	else if (n = 98)
+		printf("98");
+
+}
