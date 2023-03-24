3f95e9d21d339dcaeac9d29c02f1eb70cff0f469
rassane <an.ghassane@gmail.com>
2023-03-24 14:51:14 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * mul - show the abc's hmm
+ * @a: get corespanadin num to be mul
+ * @b: get corespanadin ascii to entered char hmm
+ * Return: 1 if lower 0 if not hmm
+ */
+void print_numbers(void);
+{
+	int i = 0;
+	while(i < 9)
+		_putchar('0' + i);
+	_putchar "(\n");
+	return;
+}
