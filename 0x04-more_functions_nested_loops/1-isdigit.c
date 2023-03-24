412dda84d0d5b86aa4aa12d9eb5f36c07ebd33e1
rassane <an.ghassane@gmail.com>
2023-03-24 14:06:17 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * isupper - show the abc's hmm
+ * isdigit - show the abc's hmm
  * @c: get corespanadin ascii to entered char hmm
  * Return: 1 if lower 0 if not hmm
  */
 int _isupper(int c)
 {
-	if (c > 64 && c < 91)
+	if (c > 47 && c < 58)
 		return (1);
 	else
 		return (0);
