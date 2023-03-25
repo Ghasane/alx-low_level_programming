8bc8d1e0d4f5047351dcc6c9a92624c8bc634679
rassane <an.ghassane@gmail.com>
2023-03-24 19:31:50 -0700

    doe

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - show the abc's hmm
+ * Return: 1 if lower 0 if not hmm
+ */
+void more_numbers(void)
+{
+	int a = 0;
+	int b = 0;
+
+	while (a <= 10)
+	{
+		while (b <= 14)
+		{
+			if (b >= 10)
+				_putchar ('1');
+			_putchar ('0' + (b % 10));
+			b++;
+		}
+		_putchar ('\n');
+		b = 0;
+		a++;
+	}
+	return;
+}
