d756dcad424038ede69f8280b65848e1a5b79951
rassane <an.ghassane@gmail.com>
2023-03-24 18:48:14 -0700

    loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,9 +5,10 @@
  * @b: get corespanadin ascii to entered char hmm
  * Return: 1 if lower 0 if not hmm
  */
-void print_numbers(void);
+void print_numbers(void)
 {
 	int i = 0;
+
 	while(i <= 9)
 	{
 		_putchar('0' + i);
