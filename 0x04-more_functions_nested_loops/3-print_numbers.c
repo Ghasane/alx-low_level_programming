263abeeb35ee8a45639f0279f385eee9e504957c
rassane <an.ghassane@gmail.com>
2023-03-24 18:57:13 -0700

    mul

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,11 +9,11 @@ void print_numbers(void)
 {
 	int i = 0;
 
-	while(i <= 9)
+	while (i <= 9)
 	{
-		_putchar('0' + i);
+		_putchar ('0' + i);
 		i++;
 	}
-	_putchar ("\n");
-	return;
+	_putchar ('\n');
+	return (0);
 }
