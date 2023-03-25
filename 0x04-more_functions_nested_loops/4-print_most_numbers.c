17fbd0b2a47b3b980c351aee1af738f1ed2de8a1
rassane <an.ghassane@gmail.com>
2023-03-24 19:10:15 -0700

    num

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_most_numbers - show the abc's hmm
+ * Return: 1 if lower 0 if not hmm
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		if (i == 2 || i ==4)
+			i++;
+		_putchar ('0' + i);
+		i++;
+	}
+	_putchar ('\n');
+	return;
+}
