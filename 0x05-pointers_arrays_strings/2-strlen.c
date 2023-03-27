d9ad8c3309ebadd61faadfb52e36367b4b1c911d
rassane <an.ghassane@gmail.com>
2023-03-27 11:19:41 -0700

    ;

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,11 @@
  * @s: is some engtry
  * Return: return lenth
  */
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int n = 0;
-	char c[]=*S;
 
-	while (c[n] != '\0')
+	while (s[n] != '\0')
 		n++;
 	return (n);
 }
