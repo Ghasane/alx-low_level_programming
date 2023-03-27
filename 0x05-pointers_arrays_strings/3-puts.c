263d5a18e3fe1bcd39656bfb1c2c545404be784f
rassane <an.ghassane@gmail.com>
2023-03-27 11:24:08 -0700

    string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - does something ...
+ * @s: is some engtry
+ * Return: return lenth
+ */
+void _puts(char *str)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+	{
+		_putchar (str[n]);
+		n++;
+	}
+	_putchar ('\n');
+	return (n);
+}
